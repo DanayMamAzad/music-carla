2e3c07cf086d8dde1f76153528e9eb2f76d62249
DaNa-00 <90591084+DanayMamAzad@users.noreply.github.com>
2022-04-11 06:17:11 +0300

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -26,7 +26,7 @@ module.exports = (client) => {
     const guildonlycounter = new Map();
     client.on("ready", () => {
         console.log(`Bot User ${client.user.username} has been logged in and is ready to use!`);
-        client.user.setActivity('&help | Hyper Canary', {
+        client.user.setActivity('+help', {
             type: 'WATCHING'
         });
         setInterval(() => {
